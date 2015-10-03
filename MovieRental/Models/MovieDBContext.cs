a9576a2d26685787c2c6dcbe00a5763543f234b4
Anna <ankol24@gmail.com>
2015-10-03 21:17:23 +0300

    clean project

---
+++ MovieRental/Models/MovieDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace MovieRental.Models
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MovieDBContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
