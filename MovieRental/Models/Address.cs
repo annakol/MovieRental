ae4e5bb3f3abe9813c97f30b40b469fad0a06b05
noylevy <levynoy12@gmail.com>
2015-10-31 23:29:26 +0200

    Address model

---
+++ MovieRental/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRental.Models
+{
+    public class Address
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string AddressStore { get; set; }
+    }
+}
